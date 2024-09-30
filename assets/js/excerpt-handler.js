62a518f9fc63cae52496d0de662366350cb8cd03
あしらーと <154391891+Asiraat@users.noreply.github.com>
2024-09-30 11:58:46 +0900

    Delete assets/js directory

--- assets/js/excerpt-handler.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const excerpts = document.querySelectorAll('.raw-excerpt');
-  
-  excerpts.forEach(excerpt => {
-    const rawHtml = excerpt.innerHTML;
-    const cleanText = rawHtml.replace(/<\/?[^>]+(>|$)/g, "");
-    excerpt.textContent = cleanText;
-  });
-});
